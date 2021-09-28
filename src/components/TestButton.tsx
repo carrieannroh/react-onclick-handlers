ddc243d003056be08f8cdfe1bca986529ffd65a7
Carrie <carriearoberts95@gmail.com>
2021-09-28 12:01:45 +0100

    committing my changes

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,5 +1,14 @@
 function TestButton(): JSX.Element {
-  return <button>Make me do something when I'm clicked...</button>;
+  //const handleClick = () => console.log("I am working!");
+  
+
+  //return <button>Make me do something when I'm clicked...</button>;
+
+  return (
+    <button
+     onClick={() => window.open("Yasss queen")}
+    >Test Button</button>
+  );
 }
 
 export default TestButton;
